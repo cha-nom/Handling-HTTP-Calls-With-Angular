4a407fb58e57384b5d2fceb4e0b1618d04f7aafd
cha-nom <ethereal.travelers@gmail.com>
2024-08-27 10:21:51 +0700

    HttpClients commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-angular-project-1';
   responseData: any;
+  loadData: boolean = false;
   constructor(private http: HttpClient){}
 
   getData(){
     this.http.get('http://jsonplaceholder.typicode.com/users').subscribe
     ((data)=>{
       this.responseData = data;
-      console.log(data);
+      this.loadData = true;
+      // console.log(data);
     })
   }
 }
